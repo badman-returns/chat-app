9a68565d2d35be7765bceed681d1f8fe0438a880
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-23 13:44:17 +0530

    feat: App ui layout created with contact and chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { useState, Fragment } from 'react'
-import './App.css'
+import { Fragment } from 'react'
+import { Grid } from '@mui/material'
+import BaseChatComponent from './components/Chat/BaseChatComponent';
+import './App.css';
+import BaseContactComponent from './components/Contacts/BaseContactComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <Fragment>
-    <h1>Gvine Chat</h1>
-   </Fragment>
+    <Fragment>
+      <div className='navbar'>
+        <h1>Gvine Chat</h1>
+      </div>
+      <Grid container className='app-container'>
+        <Grid item lg={3} className='contact-container' sx={{overflowY: 'auto'}}>
+          <BaseContactComponent/>
+        </Grid>
+        <Grid item lg={9} className='message-container'>
+          <BaseChatComponent/>
+        </Grid>
+      </Grid>
+    </Fragment>
   )
 }
 
