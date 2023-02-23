a83e14dd648f988051842dc5adea16b06b4a7ff6
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-24 04:30:15 +0530

    fix: chat section css fix

--- src/components/Contacts/ContactCard/ContactCard.tsx
+++ src/components/Contacts/ContactCard/ContactCard.tsx
@@ -1,24 +1,25 @@
 import { Avatar, Typography } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import { Fragment } from 'react'
+import { IContact } from '../../../interface/contact'
 
-export default function ContactCard() {
+export default function ContactCard({ name, picture, lastChat, latest_timestamp }: IContact) {
     return (
         <Fragment>
-            <Box display='flex' justifyContent='space-between' alignItems='center' padding='1rem' sx={{"&:hover": {background: '#35373b', transition: '0.5s', cursor: 'pointer'}}}>
+            <Box display='flex' justifyContent='space-between' alignItems='center' padding='1rem' sx={{ "&:hover": { background: '#35373b', transition: '0.5s', cursor: 'pointer' } }}>
                 <Box>
                     <Stack direction='row' gap={1}>
-                        <Avatar />
+                        <Avatar src={picture} />
                         <Box>
                             <Stack direction='column'>
-                                <Typography variant='body2' sx={{ fontWeight: '500' }}>Dan Abhrmov</Typography>
-                                <Typography variant='subtitle2' sx={{color: '#a5a5a5'}}>Some dummy messages ...</Typography>
+                                <Typography variant='body2' sx={{ fontWeight: '500' }}>{name}</Typography>
+                                <Typography variant='subtitle2' sx={{ color: '#a5a5a5' }}>{lastChat}</Typography>
                             </Stack>
                         </Box>
                     </Stack>
                 </Box>
                 <Box>
-                    <Typography variant='subtitle2' sx={{color: '#a5a5a5'}}>4:30PM</Typography>
+                    <Typography variant='subtitle2' sx={{ color: '#a5a5a5' }}>{latest_timestamp}</Typography>
                 </Box>
             </Box>
         </Fragment>
