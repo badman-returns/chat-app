62d49656be5759ad3fcd2b40201b398437edb1ea
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-25 03:31:19 +0530

    fix: initial loader improved

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+import { Grid, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+
 
 export default function Loader() {
   return (
-    <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <Grid container display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+      <Stack direction="column" gap={1} alignItems="center">
+        <Typography variant='h3'>Gvine Chat</Typography>
+        <CircularProgress color='success' />
+      </Stack>
+    </Grid>
   )
 }
 
