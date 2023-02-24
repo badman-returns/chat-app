98cf608c66c70872a43ed5061fc3fb5f8f896555
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-25 03:32:53 +0530

    fix: empty screen handling added

--- src/components/Chat/BaseChatComponent.tsx
+++ src/components/Chat/BaseChatComponent.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { useSelector } from "react-redux";
 import { IChats } from "../../interface/chat";
 import { chatSelector } from "../../store/slices/chats";
+import EmptyScreen from "../EmptyScreen/EmptyScreen";
 import ChatBody from "./ChatBody/ChatBody";
 import ChatFooter from "./ChatFooter/ChatFooter";
 import ChatHeader from "./ChatHeader/ChatHeader";
@@ -12,9 +13,13 @@ export default function BaseChatComponent() {
 
     return (
         <Fragment>
-            <ChatHeader name={name} picture={picture} />
-            <ChatBody id={id} chatlog={chatlog}/>
-            <ChatFooter />
+            {id !== 0 ? (
+                <Fragment>
+                    <ChatHeader name={name} picture={picture} />
+                    <ChatBody id={id} chatlog={chatlog} />
+                    <ChatFooter />
+                </Fragment>
+            ): <EmptyScreen/>}
         </Fragment>
     )
 }
