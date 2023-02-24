83b535fbb82f1ebb352b1b0c8b07640b26105af3
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-25 03:35:26 +0530

    fix: scroll behavior customized

--- src/components/Chat/ChatBody/ChatBody.tsx
+++ src/components/Chat/ChatBody/ChatBody.tsx
@@ -1,25 +1,32 @@
 import { Grid } from '@mui/material';
-import { Fragment, useEffect, useRef } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { ChatLog } from '../../../interface/chat';
 import './ChatBody.css';
 
 interface IChatBody {
-    id:number,
+    id: number,
     chatlog: ChatLog[]
 }
 
 export default function ChatBody({ id, chatlog }: IChatBody) {
     const messageEnd = useRef<HTMLDivElement>(null);
 
+    function scrollToBottom(behavior:any){
+        messageEnd.current?.scrollIntoView({
+            block: "end",
+            inline: "nearest",
+            behavior: behavior
+        })
+    }
+
     useEffect(() => {
-        console.log('effect');
-            messageEnd.current?.scrollIntoView({
-                block: "end",
-                inline: "nearest",
-                behavior: "auto"
-            })
+        scrollToBottom("auto");
         
-    }, [id])
+    }, [id]);
+
+    useEffect(() => {
+        scrollToBottom("smooth");
+    }, [chatlog])
 
     return (
         <Fragment>
