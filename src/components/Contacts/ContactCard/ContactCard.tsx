d293422c68f0131359d8e25d7d21da344af3a4a8
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-24 17:38:45 +0530

    feat: event handler and action attached in contact

--- src/components/Contacts/ContactCard/ContactCard.tsx
+++ src/components/Contacts/ContactCard/ContactCard.tsx
@@ -1,12 +1,20 @@
 import { Avatar, Typography } from '@mui/material'
 import { Box, Stack } from '@mui/system'
 import { Fragment } from 'react'
+import { useDispatch } from 'react-redux'
 import { IContact } from '../../../interface/contact'
+import { fetchChat } from '../../../store/slices/chats'
 
-export default function ContactCard({ name, picture, lastChat, latest_timestamp }: IContact) {
+export default function ContactCard({ id, name, picture, lastChat, latest_timestamp }: IContact) {
+    const dispatch = useDispatch();
+
+    function getChatById() {
+        dispatch(fetchChat(id));
+    }
+
     return (
         <Fragment>
-            <Box display='flex' justifyContent='space-between' alignItems='center' padding='1rem' sx={{ "&:hover": { background: '#35373b', transition: '0.5s', cursor: 'pointer' } }}>
+            <Box onClick={getChatById} display='flex' justifyContent='space-between' alignItems='center' padding='1rem' sx={{ "&:hover": { background: '#35373b', transition: '0.5s', cursor: 'pointer' }}}>
                 <Box>
                     <Stack direction='row' gap={1}>
                         <Avatar src={picture} />
