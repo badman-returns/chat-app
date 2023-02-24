4826493f6a005d42cbab3ef1027f294cd8c4085f
tsgoswami <trishnangshugoswami@yahoo.in>
2023-02-24 17:41:30 +0530

    fix: replace static data in avatar, name with mock

--- src/components/Chat/ChatHeader/ChatHeader.tsx
+++ src/components/Chat/ChatHeader/ChatHeader.tsx
@@ -3,15 +3,20 @@ import { Avatar, Grid, Typography, Box } from '@mui/material';
 import { Stack } from '@mui/system';
 import './ChatHeader.css';
 
-export default function ChatHeader() {
+interface IChatHeader{
+    name: string,
+    picture: string,
+}
+
+export default function ChatHeader({name, picture}: IChatHeader) {
     return (
         <Fragment>
             <Grid container className='chat-header-container'>
                 <Grid item >
                     <Stack direction='row' gap={2}>
-                        <Avatar alt="Remy Sharp" />
+                        <Avatar src={picture} alt="Remy Sharp" />
                         <Box display='flex' justifyContent='center' alignItems='center'>
-                            <Typography variant='h6' sx={{fontWeight: '600'}}>Dan Abhrmov</Typography>
+                            <Typography variant='h6' sx={{fontWeight: '600'}}>{name}</Typography>
                         </Box>
                     </Stack>
                 </Grid>
